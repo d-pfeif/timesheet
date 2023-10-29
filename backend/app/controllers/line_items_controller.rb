5cb8b3a6f423261e38e360b113768d604e471a08
Dakota Pfeifer <d-pfeif@outlook.com>
2023-10-28 22:26:34 -0700

    Expanded LineItemsController with create, update, destroy functionality. Includes tests.

--- backend/app/controllers/line_items_controller.rb
+++ backend/app/controllers/line_items_controller.rb
@@ -1,7 +1,41 @@
 class LineItemsController < ApplicationController
+  before_action :set_line_item, only: [:show, :update, :destroy]
+
   def index
     line_items = LineItem.where(timesheet_id: params[:timesheet_id]).order(date: :asc)
-
     render json: line_items
+  end
+
+  def create
+    line_item = LineItem.new(line_item_params)
+
+    if line_item.save
+      render json: line_item, status: :created
+    else
+      render json: { errors: line_item.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @line_item.update(line_item_params)
+      render json: @line_item
+    else
+      render json: { errors: @line_item.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @line_item.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_line_item
+    @line_item = LineItem.find_by(id: params[:id])
+  end
+
+  def line_item_params
+    params.require(:line_item).permit(:description, :date, :minutes, :timesheet_id)
   end
 end
--- backend/test/controllers/line_items_controller_test.rb
+++ backend/test/controllers/line_items_controller_test.rb
@@ -3,12 +3,16 @@ require "test_helper"
 class LineItemsControllerTest < ActionDispatch::IntegrationTest
   def setup
     @timesheet = Timesheet.create(name: "Sample Timesheet", description: "This is a sample timesheet")
+    @line_item = LineItem.create(
+      date: 1.day.ago.strftime("%Y-%m-%d"),
+      minutes: 60,
+      timesheet_id: @timesheet.id
+    )
   end
 
   test "should get index" do
     # Create some line items associated with the @timesheet
     line_item_data = [
-      { timesheet: @timesheet, date: 1.day.ago.strftime("%Y-%m-%d"), minutes: 10 },
       { timesheet: @timesheet, date: 2.days.ago.strftime("%Y-%m-%d"), minutes: 20 },
       { timesheet: @timesheet, date: 3.days.ago.strftime("%Y-%m-%d"), minutes: 30 }
     ]
@@ -27,8 +31,41 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     assert_equal 3, response_json.length
 
     # Verify they are in decending order
-    assert_equal line_item_data[2][:date], response_json[0]["date"]
-    assert_equal line_item_data[1][:date], response_json[1]["date"]
-    assert_equal line_item_data[0][:date], response_json[2]["date"]
+    assert_equal line_item_data[1][:date], response_json[0]["date"]
+    assert_equal line_item_data[0][:date], response_json[1]["date"]
+    assert_equal @line_item[:date].strftime("%Y-%m-%d"), response_json[2]["date"]
+  end
+
+  test "should create line item" do
+    assert_difference('LineItem.count') do
+      post line_items_url(@line_item), params: {
+        line_item: {
+          date: Date.today,
+          minutes: 30,
+          rate: 15.0,
+          timesheet_id: @timesheet.id
+        }
+      }
+    end
+
+    assert_response :created
+  end
+
+  test "should update line item" do
+    patch line_item_url(@line_item), params: {
+      line_item: { minutes: 100 }
+    }
+    assert_response :success
+
+    @line_item.reload
+    assert_equal 100, @line_item.minutes
+  end
+
+  test "should destroy line item" do
+    assert_difference('LineItem.count', -1) do
+      delete line_item_url(@line_item)
+    end
+
+    assert_response :no_content
   end
 end
